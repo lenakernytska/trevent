90aa82d127922526d3e45289846ef61292a6069e
lenakernytska <yaapril222@gmail.com>
2021-01-17 02:22:19 +0200

    Header

---
+++ index.js
@@ -0,0 +1,47 @@
+const refs = {
+    selectRef: document.querySelector(".header_select"),
+    selectItemRef: document.querySelector(".header_select_list"),
+    selectCurrent: document.querySelector(".header_select_current"),
+};
+
+refs.selectRef.addEventListener("click", selectHandler);
+refs.selectItemRef.addEventListener("click", selectItemHandler);
+
+
+function selectHandler() {
+    refs.selectRef.classList.toggle("is-active");
+};
+
+function selectItemHandler(event){
+    const currentText = event.target.innerHTML;
+    refs.selectCurrent.innerHTML = currentText;
+}
+    
+
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+      mobileMenuRef.classList.toggle("is-open");
+  });
+})();
+
+const navLinkRef = document.querySelector(".submenu");
+  const submenuNavRef = document.querySelector(".header_navigation_submenu_wrapper");
+console.log(navLinkRef)
+  navLinkRef.addEventListener("click", submenuNavHandler)
+    
+function submenuNavHandler(event) {
+  event.preventDefault();
+  console.log(event)
+  submenuNavRef.classList.toggle("is-open");
+}
+
+
