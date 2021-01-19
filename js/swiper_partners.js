0490f99d6fb6c391741253e80a24166c72ec5a8a
lenakernytska <yaapril222@gmail.com>
2021-01-19 20:41:46 +0200

    Final

--- js/swiper_partners.js
+++ js/swiper_partners.js
@@ -2,5 +2,16 @@
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+   },
+      slidesPerView: 1,
+      spaceBetween: 0,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
       },
     });
